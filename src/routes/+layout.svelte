13194425b22af9c008de4850225da86bb077b804
Adnan <adnan1276@proton.me>
2025-09-10 14:32:57 +0600

    installed skeleton and iconify

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script>
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-	
+	import Icon from "@iconify/svelte";
+	import { page } from '$app/stores';
 	let { children } = $props();
+
+	let scrollY = $state()
+	let innerWidth = $state()
 </script>
 
-<svelte:window></svelte:window>
+<svelte:window bind:scrollY={scrollY} bind:innerWidth={innerWidth}></svelte:window>
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
